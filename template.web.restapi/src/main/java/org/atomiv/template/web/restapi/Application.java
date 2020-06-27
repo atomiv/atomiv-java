c0633cafb90a04e54bd4a97b6d14abf2c40f44c7
Valentina Cupać <valentina.cupac@optivem.com>
2020-06-27 19:30:51 +0200

    Template - Hello world #1

---
+++ template.web.restapi/src/main/java/org/atomiv/template/web/restapi/Application.java
@@ -0,0 +1,13 @@
+package org.atomiv.template.web.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
