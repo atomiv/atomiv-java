30adc44c3955d54047c38f011f90e6f2b98df2ab
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-07-02 18:41:33 +0200

    Template - Customers - CRUD operations #3

--- template.web.restapi/src/main/java/org/atomiv/template/web/restapi/models/Customer.java
+++ template.web.restapi/src/main/java/org/atomiv/template/web/restapi/models/Customer.java
@@ -2,8 +2,39 @@ package org.atomiv.template.web.restapi.models;
 
 public class Customer {
 	
+	
+	
+	public Customer() {
+		super();
+	}
+	public Customer(long id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 	private long id;
 	private String firstName;
 	private String lastName;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 
 }
